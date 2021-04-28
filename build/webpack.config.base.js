e284f51634b4b43c7b7934eed480cb10a71a31d5
taevel02 <taevel02@gmail.com>
2021-04-29 02:21:01 +0900

    chore: correct root

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -4,7 +4,7 @@ const webpackConfig = {
 	context: __dirname,
 	entry: '../src/index.js',
 	output: {
-		path: path.resolve(`${__dirname}/dist`),
+		path: path.resolve(`${__dirname}/../dist`),
 		filename: '[name].bundle.js',
 		chunkFilename: '[name].bundle.js',
 	},
