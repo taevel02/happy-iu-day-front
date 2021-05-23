f4b5e8a1ccacd7800115dff893d5aa8587cbf8e3
taevel02 <taevel02@gmail.com>
2021-05-24 01:55:52 +0900

    feat: set up a new countdown after birthday

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 import Countdown from '../components/Countdown';
 
 function App() {
+	const year = +new Date().getFullYear();
+	const month = +new Date().getMonth() + 1;
+	const day = +new Date().getDate();
+
 	return (
 		<>
-			<Countdown date="05/16/2021" />
+			{month <= 5 && day <= 16 ? (
+				<Countdown date={`05/16/${year}`} />
+			) : (
+				<Countdown date={`05/16/${year + 1}`} />
+			)}
 		</>
 	);
 }
